4ba0c1a339bea197167a777986d03c7d32529933
William Oliveira de Lagos <william.lagos@icloud.com>
2020-04-04 23:40:12 -0300

    Added playground file for more testing

---
+++ swift.playground/Contents.swift
@@ -0,0 +1,29 @@
+import UIKit
+
+var str = "Hello, playground"
+
+var name: String = "William Lagos"
+
+var inteiro = 10
+var soma = inteiro + 3
+var a = [1,2,3]
+
+a[0]
+
+var lista = Set<String>()
+lista.insert("ABC")
+lista.insert("ABC")
+lista.insert("DEF")
+
+print(lista)
+
+var animais = [String: String]()
+
+animais["urso"] = "Animal branco que hiberna"
+animais["cachorro"] = "Melhor amigo do homem"
+
+print(animais["cachorro"]!)
+
+var idade = 22
+idade >= 18 && idade <= 26
+
