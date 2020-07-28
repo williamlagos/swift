3d69c0c893039f56dc7a8ae5e5505c56c4884679
William Oliveira de Lagos <william.lagos@icloud.com>
2020-07-28 19:24:32 -0300

    Added base source code for dog's age simple app

---
+++ Age/Age/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  age
+//
+//  Created by William Oliveira de Lagos on 28/07/20.
+//  Copyright © 2020 William Oliveira de Lagos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Age/AgeTests/AgeTests.swift
@@ -0,0 +1,34 @@
+//
+//  ageTests.swift
+//  ageTests
+//
+//  Created by William Oliveira de Lagos on 28/07/20.
+//  Copyright © 2020 William Oliveira de Lagos. All rights reserved.
+//
+
+import XCTest
+@testable import age
+
+class ageTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
