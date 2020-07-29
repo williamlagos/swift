48384c3d1b2c7a51bdc3b33a645d5d77982734e3
William Oliveira de Lagos <william.lagos@icloud.com>
2020-07-29 16:48:20 -0300

    Finished random phrases sample app

--- phrases/phrases/ViewController.swift
+++ phrases/phrases/ViewController.swift
@@ -10,11 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var phraseLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func newPhrase(_ sender: Any) {
+        var phrases: [String] = []
+        phrases.append("Se você traçar metas absurdamente altas e falhar, seu fracasso será muito melhor que o sucesso dos outros")
+        phrases.append("Ter sucesso é falhar repetidamente, mas sem perder o entusiasmo")
+        phrases.append("Não é o mais forte que sobrevive, nem o mais inteligente. Quem sobrevive é o mais disposto à mudança")
+        phraseLabel.text = phrases[Int(arc4random_uniform(3))]
+    }
+    
 }
 
