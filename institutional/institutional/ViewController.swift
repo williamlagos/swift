e8b5eeca64540eee042a5e857a98e544a822056e
William Oliveira de Lagos <william.lagos@icloud.com>
2020-07-29 18:35:29 -0300

    Added new base source code for institutional sample app

---
+++ institutional/institutional/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  institutional
+//
+//  Created by William Oliveira de Lagos on 29/07/20.
+//  Copyright © 2020 William Oliveira de Lagos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
