28a4db217e23edccc6518822733a2a69943844a9
William Oliveira de Lagos <william.lagos@icloud.com>
2020-12-24 20:15:17 +0000

    Added TableViewController to the main storyboard and some source to present

--- astro/astro/ViewController.swift
+++ astro/astro/ViewController.swift
@@ -7,8 +7,52 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
+    var signos: [String] = [
+        "Áries",
+        "Touro",
+        "Gêmeos",
+        "Câncer",
+        "Leão",
+        "Virgem",
+        "Libra",
+        "Escorpião",
+        "Sagitário",
+        "Capricórnio",
+        "Aquário",
+        "Peixes"
+    ]
+    var significadoSignos: [String] = [
+        "O ariano é uma pessoa cheia de energia e entusiasmo",
+        "...",
+        "...",
+        "...",
+        "...",
+        "...",
+        "...",
+        "...",
+        "...",
+        "...",
+        "...",
+        "...",
+    ]
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return signos.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let celulaReuso = "celulaReuso"
+        let celula = tableView.dequeueReusableCell(withIdentifier: celulaReuso, for: indexPath)
+        celula.textLabel?.text = signos[indexPath.row]
+        return celula
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
