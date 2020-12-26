925837ea01495db02ee8c51c5315d8e75a436b23
William Oliveira de Lagos <william.lagos@icloud.com>
2020-12-26 20:25:32 +0000

    Started new app related to TableView development

---
+++ movies/movies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  movies
+//
+//  Created by William Oliveira de Lagos on 24/04/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
